97f51c99e0a053b526359acd590468d575a0dd1f
MushtaqAli1997 <121460506+MushtaqAli1997@users.noreply.github.com>
2023-05-08 03:37:44 +0530

    Delete assinment1 directory

--- assinment1/src/assinment1/SkyDriver.java
+++
@@ -1,9 +0,0 @@
-package assinment1;
-
-public class SkyDriver {
-
-	public static void main(String[] args) {
-		SkyscraperConstruction skyscrapper = new SkyscraperConstruction();
-		skyscrapper.input();
-	}
-}
