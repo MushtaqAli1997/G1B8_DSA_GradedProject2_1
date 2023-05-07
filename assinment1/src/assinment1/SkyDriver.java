dde55183edbf671c7d27d31645589ce2fbc9b846
MushtaqAli1997 <121460506+MushtaqAli1997@users.noreply.github.com>
2023-05-08 03:39:40 +0530

    Add files via upload

---
+++ assinment1/src/assinment1/SkyDriver.java
@@ -0,0 +1,9 @@
+package assinment1;
+
+public class SkyDriver {
+
+	public static void main(String[] args) {
+		SkyscraperConstruction skyscrapper = new SkyscraperConstruction();
+		skyscrapper.input();
+	}
+}
